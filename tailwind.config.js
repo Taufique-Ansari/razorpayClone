e4304492103fd4c81a762d9b8195957f7f34c851
Taufique-Ansari <Taufiqueansari451@gmail.com>
2023-07-09 20:44:22 +0530

    resolved responsiveness issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       boxShadow: {
         '3xl': '2px -2px 50px -5px rgba(0,0,0,0.75)',
         'nm' : '0px 0px 5px 0px rgba(235,226,235,1)',
-      }
+      },
+      
     },
   },
   plugins: [],
