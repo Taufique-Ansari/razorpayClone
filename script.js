91c27563cc2642a66af17d739ce484f241d84632
Taufique-Ansari <Taufiqueansari451@gmail.com>
2023-07-14 22:37:49 +0530

    navbar mod

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var sideBar = document.querySelector('.side-bar');
 var humbuger = document.querySelector('.humburger');
 var cross = document.querySelector('.cross');
-
+var dash = document.querySelector('.nav-dash');
+var navEle = document.querySelector('.nav-ele1');
+var navCtn = document.querySelector('.nav-ele1-ctn');
 
 humbuger.addEventListener('click', ()=> {
     console.log("hello");
@@ -12,3 +14,9 @@ cross.addEventListener('click', ()=>{
     console.log("2hello");
     sideBar.classList.add('hide-sidebar');
 })
+
+// navEle.addEventListener("mouseover",()=>{
+//     console.log("Hi");
+//     navCtn.classList.toggle('show-dropdwn');
+//     dash.classList.toggle('show-dropdwn');
+// })
